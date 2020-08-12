7ef1e42dc2dde9549f4da987b39885bb9ddadddc
unknown <guivecchi12@gmail.com>
2020-08-11 21:49:36 -0600

    start

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code away!
+const express = require("express")
+
+const logger = require("./middleware/logger")
+const userRouter = require("./users/userRouter")
+
+const server = express()
+const port = 4000
+
+server.use(express.json())
+
+server.use(logger())
+server.use(userRouter)
+
+
+server.use((err, req, res, next) => {
+	// log the error and return a generic response to avoid the risk
+	// of leaking sensitive info that might be in the error
+	console.log(err)
+
+	res.status(500).json({
+		message: "Something went wrong, try again later",
+	})
+})
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`)
+})
